8e81ce5d03729898efbef8f95f24173ad85deb44
Reuben Addo <adoteythefirst17@gmail.com>
2025-08-03 16:21:55 +0100

    The code is finished and it is running

--- TriangleIdentifier/Program.cs
+++ TriangleIdentifier/Program.cs
@@ -4,7 +4,27 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Triangletype triangle = new Triangletype();
+            Console.WriteLine("Enter the first side of the triangle below");
+            int firstSide = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the second side of the triangle below");
+            int secondSide = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the last side of the triangle below to determine the type");
+            int thirdSide = int.Parse(Console.ReadLine());
+
+            if (firstSide <=0 || secondSide<=0 || thirdSide<=0)
+            {
+                Console.WriteLine("The sides of a triangle must be greater than zero. Please enter valid side lengths.");
+                Console.WriteLine("Re-enter the first side of the triangle below");
+                int firstSide1 = int.Parse(Console.ReadLine());
+                Console.WriteLine("Re-nter the second side of the triangle below");
+                int secondSide1 = int.Parse(Console.ReadLine());
+                Console.WriteLine("Re-enter the last side of the triangle below to determine the type");
+                int thirdSide1 = int.Parse(Console.ReadLine());
+                triangle.IdentifyTriangle(firstSide1, secondSide1, thirdSide1);
+            }
+
+            triangle.IdentifyTriangle(firstSide, secondSide, thirdSide);
         }
     }
 }
--- TriangleIdentifier/Triangletype.cs
+++ TriangleIdentifier/Triangletype.cs
@@ -6,7 +6,39 @@ using System.Threading.Tasks;
 
 namespace TriangleIdentifier
 {
-    internal class triangletype
+    internal class Triangletype
     {
+
+        public int Side1 { get; set; }
+        public int Side2 { get; set; }
+        public int Side3 { get; set; }
+
+        public string TriangleName = "";
+
+        public string IdentifyTriangle(int Side1, int Side2, int Side3)
+        {
+            if (Side1 == Side2 && Side2 == Side3)
+            {
+                TriangleName = "Equilateral Triangle";
+                Console.WriteLine("All the sides are equal so therefore it is an: " + TriangleName);
+            }
+            else if (Side1 == Side2 || Side2 == Side3 || Side1 == Side3)
+            {
+                TriangleName = "Isosceles Triangle";
+                Console.WriteLine("Two sides are equal so therefore it is an: " + TriangleName);
+            }
+            else if (Side1 != Side2 && Side2 != Side3 && Side1 != Side3)
+            {
+                TriangleName = "Scalene Triangle";
+                Console.WriteLine("All the sides are different so therefore it is a: " + TriangleName);
+            }
+
+            return TriangleName;
+            {
+
+            }
+
+        }
     }
 }
+
