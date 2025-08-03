314751fbe715dce700b5953c855996f60a287fef
Reuben Addo <adoteythefirst17@gmail.com>
2025-08-03 03:47:01 +0100

    Testing the code

---
+++ GradeCalc/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace GradeCalc
+{
+    internal class Program
+    {
+        public  int GradeValue { get; set; }
+
+        private static int GetGradeValue()
+        {
+            return GradeValue;
+        }
+
+        static void Main(string[] args, int gradeInput)
+        {
+            Grade grade = new Grade();
+
+            Console.WriteLine("Enter your grade value:");
+            gradeInput = int.Parse(Console.ReadLine());
+            //Console.WriteLine(int.TryParse(Console.ReadLine(), result: out Program.GradeValue);
+        }
+    }
+}
