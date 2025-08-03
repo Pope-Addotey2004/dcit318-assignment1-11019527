aab46e9874f9e6934ad877391418289c8a9e4450
Reuben Addo <adoteythefirst17@gmail.com>
2025-08-03 04:49:28 +0100

    the code works perfectly

--- GradeCalc/Grade.cs
+++ GradeCalc/Grade.cs
@@ -6,7 +6,43 @@ using System.Threading.Tasks;
 
 namespace GradeCalc
 {
-    internal class Class1
+    
+    internal class Grade
     {
+        public int GradeValue { get; set; } =  int.Parse(Console.ReadLine());
+        public string GradeLetter { get; set; }
+
+        public  string  GradeCalculation(int gradeValue)
+        {
+            if (gradeValue >= 90)
+            {
+                GradeLetter = "A";
+                Console.WriteLine("You got an A!");
+            }
+            else if (gradeValue >= 80)
+            {
+                GradeLetter = "B";
+                Console.WriteLine("You got a B!");
+            }
+            else if (gradeValue >= 70)
+            {
+                GradeLetter = "C";
+                Console.WriteLine("You got a C!");
+            }
+            else if (gradeValue >= 60)
+            {
+                GradeLetter = "D";
+                Console.WriteLine("You got a D!");
+            }
+            else if (gradeValue < 60)
+            {
+                GradeLetter = "F";
+                Console.WriteLine("You got an F!");
+            }
+            
+
+                return GradeLetter;
+            
+        }
     }
 }
--- GradeCalc/Program.cs
+++ GradeCalc/Program.cs
@@ -2,20 +2,26 @@
 {
     internal class Program
     {
-        public  int GradeValue { get; set; }
+        public int GradeValue { get; set; }
 
-        private static int GetGradeValue()
+        static void Main(string[] args)
         {
-            return GradeValue;
-        }
-
-        static void Main(string[] args, int gradeInput)
-        {
-            Grade grade = new Grade();
+            Grade grade = new();
 
             Console.WriteLine("Enter your grade value:");
-            gradeInput = int.Parse(Console.ReadLine());
-            //Console.WriteLine(int.TryParse(Console.ReadLine(), result: out Program.GradeValue);
+            int gradeInput = int.Parse(Console.ReadLine());
+
+            if (gradeInput < 0 || gradeInput > 100)
+            {
+                Console.WriteLine("Invalid grade value. Please enter a value between 0 and 100.");
+                gradeInput = int.Parse(Console.ReadLine());
+                grade.GradeCalculation(gradeInput);
+                return;
+            }
+            
+            grade.GradeCalculation(gradeInput);
+            Console.WriteLine(grade.GradeLetter);
+            
         }
     }
 }
