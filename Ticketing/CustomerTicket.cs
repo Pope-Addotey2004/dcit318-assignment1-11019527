af3b3f0121b69f955a7ef4a2cba9fc25b5a9dced
Reuben Addo <adoteythefirst17@gmail.com>
2025-08-03 05:34:00 +0100

    Finished with the ticketing application

--- Ticketing/CustomerTicket.cs
+++ Ticketing/CustomerTicket.cs
@@ -6,7 +6,31 @@ using System.Threading.Tasks;
 
 namespace Ticketing
 {
+    
     internal class CustomerTicket
     {
+        public int TicketPrice { get; set; } = 10;
+        public int NewPrice { get; set; }
+
+        public int CustomerAge { get; set; }
+
+        public  int TicketingScheme(int TicketPrice, int CustomerAge)
+        {
+            if (CustomerAge>=65 || CustomerAge<=12)
+            {
+                NewPrice = TicketPrice - 3;
+                
+                Console.WriteLine(NewPrice.ToString() + "cedis");
+            }
+            else
+            {
+                 NewPrice = TicketPrice;
+                Console.WriteLine(NewPrice.ToString() + "cedis");
+            }
+
+            return NewPrice;
+        }
+
+        
     }
 }
--- Ticketing/Program.cs
+++ Ticketing/Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            CustomerTicket ticket = new CustomerTicket();
+
+            Console.WriteLine("Customer should please enter His/Her Age: ");
+            int Age = int.Parse(Console.ReadLine());
+
+            if (Age < 0)
+            {
+                Console.WriteLine("Age cannot be negative. Please enter a valid age.");
+                int Age1 = int.Parse(Console.ReadLine());
+                ticket.TicketingScheme(ticket.TicketPrice, Age1);
+            }
+
+            ticket.TicketingScheme(ticket.TicketPrice, Age);
+            //Console.WriteLine(ticket.NewPrice.ToString() + "cedis");
+
         }
     }
 }
